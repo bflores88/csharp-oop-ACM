aeb10aa48bbe5a3ea3e083665ae6830c67a0a58a
Brenda Flores <kmietotwy@gmail.com>
2019-12-31 20:18:55 -0800

    wrote unit test to see polymorphism of ILoggable in action

--- Acme.Common/LoggingService.cs
+++ Acme.Common/LoggingService.cs
@@ -8,11 +8,11 @@ namespace Acme.Common
 {
     public static class LoggingService
     {
-        public static void WriteToFile(List<Object> itemsToLog)
+        public static void WriteToFile(List<ILoggable> itemsToLog)
         {
             foreach (var item in itemsToLog)
             {
-                //Console.WriteLine();
+                Console.WriteLine(item.Log());
             }
         }
     }
---
+++ Acme.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using ACM.BL;
+using Acme.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class LoggingServiceTest
+    {
+        [TestMethod]
+        public void WriteToFileTest()
+        {
+            //-- Arrange
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins",
+                EmailAddress = "bBaggins@hobbit.me",
+                AddressList = null
+            };
+            changedItems.Add(customer);
+
+            var product = new Product(2)
+            {
+                ProductName = "Rake",
+                ProductDescription = "Garden Rake with Steel Head",
+                CurrentPrice = 10
+            };
+            changedItems.Add(product);
+
+            //-- Act
+            LoggingService.WriteToFile(changedItems);
+
+            //-- Assert
+            // Nothing here to assert
+        }
+    }
+}
